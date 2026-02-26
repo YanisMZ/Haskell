1406d92725a59d8a579db3f048f64440eacc350e
yanis <yanismzyene7@gmail.com>
2026-02-26 10:03:49 +0100

    revsion

--- tp2/tp2.hs
+++ tp2/tp2.hs
@@ -19,3 +19,39 @@ interval' :: (Num a, Ord a) => a -> a -> [a]
 interval' s k
   | s > k     = []
   | otherwise = s : interval' (s + 1) k
+
+
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome xs = xs == reverse xs
+
+
+pairs :: [a] -> [(a,a)]
+pairs [] = []
+pairs [a] = []
+pairs (x:y:xs) = (x, y) : pairs (y:xs)
+
+
+prefixes :: [a] -> [[a]]
+prefixex [] = [[]]
+prefixes [a] = [[a]]
+prefixes xs = prefixes(init xs) ++ [xs]
+
+
+factors :: [a] -> [[a]]
+factors [] = [[]]
+factors [a] = [[a],[]]
+factors xs = prefixes(xs) ++ factors(tail xs)
+
+
+subseqs :: [a] -> [[a]]
+subseqs [] = [[]]
+subseqs xs = filterM (return[True,False])
+
+
+isArithSerie :: (Eq a, Num a) => [a] -> Bool
+isArithSerie [] = True
+isArithSerie [_] = True
+isArithSerie [_, _] = True
+isArithSerie (x:y:z:xs) = (y - x == z - y) && isArithSerie (y:z:xs)
+
--- tp3/revision.hs
+++ tp3/revision.hs
@@ -17,8 +17,6 @@ fb n
   | otherwise       = show n
 
 
-
-
 altParity3 :: Int -> Int -> [(Int, Int, Int)]
 altParity3 fromN toN =
   [ (x,y,z)
@@ -29,9 +27,6 @@ altParity3 fromN toN =
   , not (x == y && y == z)
   , not (even x == even y && even y == even z)
   ]
-
-
-
 
 withAtLeastKLetters :: [String] -> Int -> [String]
 withAtLeastKLetters xs k = [word | word <- xs , length word >= k]
@@ -46,5 +41,102 @@ countVowels :: String -> Int
 countVowels word = length [c | c <- word, c `elem` "aeiouy"]
 
 
+any' :: (a -> Bool) -> [a] -> Bool
+any' f [] = False
+any' f (x:xs) = if f x then True else any' f xs
 
 
+any'' :: (a -> Bool) -> [a] -> Bool
+any'' f xs = foldr(\x acc -> if f x then True else acc ) False xs
+
+
+any''' :: (a -> Bool) -> [a] -> Bool
+any''' f xs = foldr fb False xs
+  where
+    fb x acc = if f x then True else acc
+
+
+group' :: (Eq a) => [a] -> [[a]]
+group' xs = foldr (\x acc -> f x acc) [] xs
+  where 
+    f x [] = [[x]]
+    f x (y:ys) 
+        | x == head y = (x:y) : ys
+        | otherwise   = [x] : y : ys
+
+
+
+while :: (a -> Bool) -> (a -> a) -> a -> [a]
+while p f x
+  | p x       = x : while p f (f x)
+  | otherwise = []
+
+
+
+
+
+intersectI :: (Int, Int) -> (Int, Int) -> Bool
+intersectI (a,b) (c,d) = a <= d && c <= b
+
+
+
+isLeftSortedI :: [(Int, Int)] -> Bool
+isLeftSortedI [] = True
+isLeftSortedI [_] = True
+isLeftSortedI ((a,_):(b,_):xs) =
+  a <= b && isLeftSortedI ((b,0):xs)
+
+
+
+sublists :: Int -> [a] -> [[a]]
+sublists 0 _      = [[]]
+sublists _ []     = []
+sublists k (x:xs) =
+  map (x:) (sublists (k-1) xs)
+  ++ sublists k xs
+
+
+
+
+
+cycle' :: [a] -> [a]
+cycle' [] = error "cycle': empty list"
+cycle' xs = xs ++ cycle' xs
+
+
+cycle'' :: [a] -> [a]
+cycle'' [] = error "cycle': empty list"
+cycle'' xs = f xs
+  where
+    f [] = f xs
+    f (y:ys) = y : f ys
+
+
+
+
+
+sumOddsEvens :: [Int] -> (Int, Int)
+sumOddsEvens xs = (sum odds, sum evens)
+  where
+    odds  = [x | x <- xs, odd x]
+    evens = [x | x <- xs, even x]
+
+
+
+
+
+oneOutOfEveryTwo :: [a] -> [a]
+oneOutOfEveryTwo []       = []
+oneOutOfEveryTwo [x]      = [x]
+oneOutOfEveryTwo (x:_:xs) = x : oneOutOfEveryTwo xs
+
+
+
+
+oneOutOfEveryK :: Int -> [a] -> [a]
+oneOutOfEveryK k xs = [x | (x,i) <- zip xs [0..], i `mod` k == 0]
+
+
+
+increasingPairs :: Int -> Int -> [(Int, Int)]
+increasingPairs min max = [(x,y) | x <-[min..max], y <- [min..max], x<y,even (x+y)]
